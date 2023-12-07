0618c586ac654c193bfe4bbea8422b3fc1feeaa6
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-12-07 16:46:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,16 @@ function App() {
   const toggleCamera = () => {
     setIsWorking(!isWorking)
   }
+  
+  const sectionClass = isWorking ? 'bg-gradient-to-tr from-green-400 to-purple-700' : 'bg-gray-800'
 
   return (
     <>
-      <main className='flex flex-col w-full h-screen text-green-300 bg-gray-900 justify-center items-center bg-gradient-to-tr from-purple-800 to-green-400 via-gray-600'>
-        <section className="relative w-96 min-h-[506px] max-h-[506px] my-auto border border-green-300 shadow-2xl rounded-xl p-2 bg-gray-800">
+      <main className='relative flex flex-col w-full h-screen text-green-300 bg-gray-900 justify-center items-center bg-gradient-to-tr from-purple-700 to-green-400'>
+        {!isWorking&&
+          <h1 className='absolute top-2 text-lg text-gray-800 mt-3'>Включи, чтобы увидеть самого прекрасного человека!</h1>
+        }
+        <section className={`relative w-96 min-h-[506px] max-h-[506px] my-auto border border-green-300 shadow-2xl rounded-xl p-2 ${sectionClass}`}>
           <button
             className="bg-gray-700 p-2 m-2 rounded-xl absolute z-10 min-w-[48px] uppercase right-1 top-1"
             onClick={toggleCamera}
