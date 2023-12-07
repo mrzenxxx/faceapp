53ac17e2538a4f3176b459bfa76be45ce9b6adf8
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-12-07 23:36:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
 
   return (
     <>
-      <main className='relative flex flex-col w-full h-screen text-green-300 bg-gray-900 justify-center items-center bg-gradient-to-tr from-purple-700 to-green-400'>
-        <section className={`relative w-[380px] min-h-[380px] max-h-[380px] my-auto border border-green-300 shadow-2xl rounded-xl flex jusify-center items-center bg-gray-800`}>
+      <main className='relative flex flex-col w-full h-screen text-gray-300 bg-gray-900 justify-center items-center'>
+        <section className={`relative w-[380px] min-h-[380px] max-h-[380px] my-auto rounded-2xl flex jusify-center items-center bg-gray-800`}>
           <button
             className="bg-gray-700 p-2 m-2 rounded-xl absolute z-10 min-w-[48px] uppercase right-1 top-1"
             onClick={toggleCamera}
@@ -22,12 +22,11 @@ function App() {
           {isWorking &&
             <>
               <Webcam
-                className="w-full h-full rounded-xl"
+                className="w-full h-full rounded-2xl"
                 videoConstraints={webCamConfig}
                 mirrored 
               />
-              <h1 className='absolute text-lg top-[-3rem] text-purple-800 text-bold text-center mx-auto w-full uppercase animate-ping'>ты</h1>
-              <h1 className='absolute text-lg bottom-[-5rem] text-green-300 text-bold text-center mx-auto w-full uppercase animate-ping'>самый прекрасный <br/>человек</h1>
+              <h1 className='absolute text-xl bottom-5 text-gray-500 text-bold text-center mx-auto w-full uppercase animate-ping'>чушпан</h1>
             </>
           }
         </section>
