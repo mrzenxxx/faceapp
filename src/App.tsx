f4fa19e30039a4d42ff728b39ad01137ea7a4eb6
Archi Gene <44780361+mrzenxxx@users.noreply.github.com>
2023-12-07 15:02:39 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+      <main className='flex w-full h-screen text-green-300 bg-gray-700 justify-center items-center'>
+        <h1 className="uppercase">hello!</h1>
+      </main>
+  )
+}
+
+export default App
